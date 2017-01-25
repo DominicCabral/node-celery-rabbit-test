698683efcadef8bce8f2dd432230d8a65a330019
Dominic Cabral <dcabral@netra.io>
2017-01-25 09:56:26 -0500

    modified to call from browser to simulate IRIS

--- producer/index.js
+++ producer/index.js
@@ -1,6 +1,6 @@
 var celery = require('node-celery');
-
-// Initialize connection to rabbitmq
+var express = require('express');
+var app = express();
 var client = celery.createClient({
     CELERY_BROKER_URL: 'amqp://guest:guest@rabbitMQ//',
     CELERY_ROUTES: {
@@ -11,8 +11,13 @@ var client = celery.createClient({
     IGNORE_RESULT: true
 });
 
+app.listen(80, function () {
+  console.log('app listening on port 80')
+})
+
+//visit 0.0.0.0 in browser to call
 client.on('connect', () => {
-    setInterval(sendMessageToWorker, 1000);
+    app.get('/', sendMessageToWorker);
 });
 
 client.on('error', function (err) {
@@ -20,11 +25,11 @@ client.on('error', function (err) {
 });
 
 
-function sendMessageToWorker() {
+function sendMessageToWorker(req,res) {
     const message = { hello: 'wabbit' };
 
-    console.log('connected');
     var task = client.createTask("worker.run");
     task.call([message]);
+    res.send('sent');
 }
 
