a26a7d6d4ab4deb9dc618c38c5d74f7e9614972f
dominiccabral <dcabral@netra.io>
2017-03-29 13:28:45 -0400

    add 2nd producer

--- consumer/worker.py
+++ consumer/worker.py
@@ -2,9 +2,9 @@ from celery import Celery
 from time import sleep
 
 app = Celery('tasks', broker='amqp://guest:guest@rabbitMQ//')
-app.conf.CELERYD_PREFETCH_MULTIPLIER = 1
+app.conf.task_queue_max_priority = 10
 
 @app.task(bind=True)
 def run(self, msg):
-    sleep(1)
+    sleep(2)
     print(msg)
--- producer/index.js
+++ producer/index.js
@@ -1,30 +1,41 @@
 var celery = require('node-celery');
+var importantCounter = 0;
+var normalCounter = 0;
 
 // Initialize connection to rabbitmq
 var client = celery.createClient({
-    CELERY_BROKER_URL: 'amqp://guest:guest@rabbitMQ//',
-    CELERY_ROUTES: {
-        'worker.run': {
-            queue: 'tasks'
-        }
-    },
-    IGNORE_RESULT: true
+  CELERY_BROKER_URL: 'amqp://guest:guest@rabbitMQ//',
+  CELERY_ROUTES: {
+    'worker.run': {
+      queue: 'tasks'
+    }
+  },
+  IGNORE_RESULT: true
 });
 
 client.on('connect', () => {
-    setInterval(sendMessageToWorker, 1000);
+  setInterval(sendMessageToWorker, 500);
+  setInterval(sendImportantMessageToWorker, 5000);
 });
 
-client.on('error', function (err) {
-    console.log(err);
+client.on('error', function(err) {
+  console.log(err);
 });
 
 
 function sendMessageToWorker() {
-    const message = { hello: 'wabbit' };
-
-    console.log('connected');
-    var task = client.createTask("worker.run");
-    task.call([message]);
+  const message = { normal: normalCounter };
+  console.log(normalCounter)
+  var task = client.createTask("worker.run");
+  task.call([message], {}, { priority: 1 });
+  normalCounter++;
 }
 
+function sendImportantMessageToWorker() {
+
+  console.log('IMPORTANT', importantCounter)
+  const message = { important: importantCounter };
+  var task = client.createTask("worker.run");
+  task.call([message], {}, { priority: 10 });
+  importantCounter++;
+}
